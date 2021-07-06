4806a466884818badf0b4e6c00d8510ae15ac9ba
pgm-arthtemm <arthur.temmerman@student.arteveldehs.be>
2021-07-06 13:00:19 +0200

    made fetch example

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-console.log('Hello World!');
+(() => {
+  const API_URL = 'https://api.punkapi.com/v2/beers';
+  const app = {
+    init() {
+      this.fetchData();
+    },
+    async fetchData() {
+      const response = await fetch(API_URL);
+      const data = await response.json();
+      this.populateHTML(data);
+    },
+    populateHTML(data) {
+      console.log(data)
+    }
+  };
+  app.init();
+})();

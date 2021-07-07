688e2512f615df50fff4e3fd162472c076246199
Michiel Leyman <michiel.leyman@gmail.com>
2021-07-07 16:52:50 +0200

    feat: allow cors requests

--- backend/src/api.py
+++ backend/src/api.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
 
     @app.route("/api/hello", methods=["GET"])
     def sample_route():

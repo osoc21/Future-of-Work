025103df37217da73d902dd21ae7812872d9fd48
Michiel Leyman <michiel.leyman@gmail.com>
2021-07-08 15:03:15 +0200

    style: add and configure eslint & prettier

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: ['eslint:recommended', 'prettier'],
+  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  rules: {
+    'prettier/prettier': 'error'
+  }
+};
--- frontend/js/app.js
+++ frontend/js/app.js
@@ -8,65 +8,66 @@ const fetchData = async () => {
   const response = await fetch(API_URL);
   const data = await response.json();
   convertData(data);
-}
+};
 fetchData();
 
 // Converting json data to arrays
 const convertData = (data) => {
-  let labels = data.results.map(function(e) {
-    return e.id
-  })
+  let labels = data.results.map(function (e) {
+    return e.id;
+  });
 
-  let values = data.results.map(function(e) {
+  let values = data.results.map(function (e) {
     return e.value;
-  })
-  
+  });
+
   generateCharts(labels, values);
-}
+};
 
 const generateCharts = (labels, values) => {
   // Cache myChart element
-let myChart = document.getElementById('myChart').getContext('2d');
+  let myChart = document.getElementById('myChart').getContext('2d');
 
-// Init Chart.js and create chart using parameters
-let landingPageRating = new Chart(myChart, {
-  type: 'bar',
-  data: {
-    labels:labels,
-    datasets:[{
-      label:'Amount of votes',
-      data:values,
-      backgroundColor: 'rgb(160, 220, 100)',
-      borderWidth: 1,
-      borderColor: '#777',
-      hoverBorderWidth: 3,
-      hoverBorderColor: '#000'
-    }]
-  },
-  options: {
-    title: {
-      display: true,
-      text: 'Landing page rating (out of 10)'
-    },
-    scales: {
-      y : {
-        max: 20,
-        min: 0,
-        ticks: {
-          stepSize : 1
+  // Init Chart.js and create chart using parameters
+  let landingPageRating = new Chart(myChart, {
+    type: 'bar',
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: 'Amount of votes',
+          data: values,
+          backgroundColor: 'rgb(160, 220, 100)',
+          borderWidth: 1,
+          borderColor: '#777',
+          hoverBorderWidth: 3,
+          hoverBorderColor: '#000'
         }
-      }
+      ]
     },
-    plugins: {
-      legend: {
-        labels: {
-          font: {
-            size: 20
+    options: {
+      title: {
+        display: true,
+        text: 'Landing page rating (out of 10)'
+      },
+      scales: {
+        y: {
+          max: 20,
+          min: 0,
+          ticks: {
+            stepSize: 1
+          }
+        }
+      },
+      plugins: {
+        legend: {
+          labels: {
+            font: {
+              size: 20
+            }
           }
         }
       }
     }
-  }
-})
-}
-
+  });
+};

2c7ad26fcb5d3ac7ffe9790829817a935a8d4623
Erinn Van der Sande <erinn.francis.van.der.sande@vub.be>
2021-07-14 11:08:37 +0200

    added some extra lines to the request-parser

--- backend/src/api.py
+++ backend/src/api.py
@@ -84,7 +84,9 @@ def create_app():
             """
             # check if the post request has the file part
             parse = reqparse.RequestParser()
-            parse.add_argument('file', type= werkzeug.datastructures.FileStorage, location='files')
+            parse.add_argument('Population.csv', type= werkzeug.datastructures.FileStorage, location='files')
+            parse.add_argument('Attrition.csv', type= werkzeug.datastructures.FileStorage, location='files')
+            parse.add_argument('Retirement.csv', type= werkzeug.datastructures.FileStorage, location='files')
             args = parse.parse_args()
 
             abort(400,message="Population is missing got " + str(args))

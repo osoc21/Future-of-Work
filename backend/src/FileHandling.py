2880784b9a86645d519d7dca413a90bf47bac90a
Erinn Van der Sande <erinn.francis.van.der.sande@vub.be>
2021-07-15 10:34:38 +0200

    Global ID as result from  upload post request

--- backend/src/FileHandling.py
+++ backend/src/FileHandling.py
@@ -1,10 +1,11 @@
 from uuid import uuid1
 import pandas as pd 
 import io
+import json
 
 def writeCSV(files,names,redis):
-    result = {'globalID':str(uuid1())}
     globalID = str(uuid1())
+    result = {}
     for file,name in zip(files,names):
         df = pd.read_csv(file)
         ids = []
@@ -12,12 +13,12 @@ def writeCSV(files,names,redis):
         for _,row in df.iterrows():
             id = str(uuid1())
             ids.append(id) 
-            redis.set(id,row.to_json()) 
-        df['id'] = ids 
+            redis.set(id,row.to_json())
         redis.set(dataID,str(ids)) 
-        result[name] = {'id':dataID,'Data':df.to_dict()}
-    return result
-
+        result[name] = {'id':dataID} 
+    redis.set(globalID,json.dumps(result,indent=0))
+    return globalID
+ 
 def readCSV(id,name,redis):
     buffer = io.BytesIO(redis.get(str(id) + "-" + name))
     buffer.seek(0)
--- backend/src/api.py
+++ backend/src/api.py
@@ -78,24 +78,34 @@ def create_app():
                 400:
                     description: form is incorrect format could not find poplation file
                 401:
-                    description: 
-                401:
-                    description: One of the files is empty meaning no file was sent in
+                    description: form is incorrect format could not find attrition file
                 402:
-                    description: One of the files isn't a csv
+                    description: form is incorrect format could not find retirement file
+                410:
+                    description: population file is empty so no file was selected
+                411:
+                    description: attrition file is empty so no file was selected
+                420:
+                    description: population file isn't a csv
+                421:
+                    description: attrition file isn't a csv
+                422:
+                    description: retirement file isn't a csv
                 200:
-                    description: succes returns a json with the csv
+                    description: succes returns a global ID where the files are stored
+                500:
+                    description: internal server error
             """
             # check if the post request has the file part
             if 'population' not in request.files:
                 flash('population is missing')
-                abort(400,message="Population is missing got " + str(request.files.keys()))
+                abort(400,message="form is incorrect format could not find population, files found:" + str(request.files.keys()))
             elif 'attrition' not in request.files:
                 flash('attrition is missing')
-                abort(401,message="Attrition is missing" + str(request.files))
+                abort(401,message="form is incorrect format could not find attrition file, files found:" + str(request.files.keys()))
             elif 'retirement' not in request.files:
                 flash('Retirment is missing')
-                abort(402,message="Retirement is missing" + str(request.files))
+                abort(402,message="form is incorrect format could not find retirement file, files found:" + str(request.files.keys()))
             else:
                 populationFile = request.files['population']
                 attritionFile = request.files['attrition']
@@ -104,29 +114,29 @@ def create_app():
                 # empty file without a filename.
                 if populationFile.filename == '':
                     flash('No Population file selected')
-                    abort(410)
+                    abort(410,message="population file is empty so no file was selected")
                 elif attritionFile.filename == '':
                     flash('No Attrition file selected')
-                    abort(411)
+                    abort(411,message ="attrition file is empty so no file was selected")
                 elif retirementFile.filename == '':
                     flash('No Retirement file selected')
-                    abort(412)
+                    abort(412,message="attrition file is empty so no file was selected")
                 elif populationFile and attritionFile and retirementFile and map(lambda x: allowed_file(x,{'csv'}),[populationFile,attritionFile,retirementFile]):
                     if not(allowed_file(populationFile.filename,{'csv'})):
                         flash('population is not a csv')
-                        abort(420)
+                        abort(420,message="population file is not a csv")
                     elif not(allowed_file(attritionFile.filename,{'csv'})):
                         flash('attrition is not a csv')
-                        abort(421)
+                        abort(421,message="attrition file is not a csv")
                     elif not(allowed_file(retirementFile.filename,{'csv'})):
                         flash('retirement is not a csv')
-                        abort(422)
+                        abort(422,message="retirement file is not a csv")
                     else: 
-                        result = writeCSV([populationFile,attritionFile,retirementFile],["population","attrition","retirement"],r)
-                        return {"result" : result}, 200
+                        globalID = writeCSV([populationFile,attritionFile,retirementFile],["population","attrition","retirement"],r)
+                        return {"ID" : globalID}, 200
                 else:
                     flash('Internal Error')
-                    abort(500)
+                    abort(500,message="Internal server error")
 
     class LoadFile(Resource):
         def PUT(self):

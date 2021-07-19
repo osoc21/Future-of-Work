352b37826f48024f7815e3850cf150fa2dad826a
Erinn Van der Sande <erinn.francis.van.der.sande@vub.be>
2021-07-19 16:20:20 +0200

    I calculate the current FTE of each year minus the retirement

--- backend/src/supply.py
+++ backend/src/supply.py
@@ -2,47 +2,81 @@ import pandas as pd
 import numpy as np
 import datetime 
 
+format = "%d.%m.%Y"
+
 def createDF(csvs):
     attritionDf = pd.DataFrame(csvs["attrition"])
     populationDf = pd.DataFrame(csvs["population"])
-    retirementDf = pd.DataFrame(csvs["retirement"]) 
+    retirementDf = pd.DataFrame(csvs["retirement"])
     
     del attritionDf["rowID"]
     del populationDf["rowID"]
-    del retirementDf["rowID"] 
+    del retirementDf["rowID"]
 
-    result = populationDf 
+    populationDf["Birth date"] = pd.to_datetime(populationDf["Birth date"], format=format)
+
+    result = populationDf
 
     def attrition(row):
         if row["Country of Personnel Area"] in attritionDf.columns:
-            val = attritionDf.loc[attritionDf["Job Family"] == row["Job Family"]][row["Country of Personnel Area"]].values
-            if val.empty:
-                print(val)
-                print(row)
+            country = row["Country of Personnel Area"]
+            jobFamily = row["Job Family"]
+            val = attritionDf.loc[attritionDf["Job Family"] == jobFamily][country].values[0]
         else:
-            print(attritionDf.loc[attritionDf["Job Family"] == row["Job Family"]]["Others"])
-            val = attritionDf.loc[attritionDf["Job Family"] == row["Job Family"]]["Others"]
-        return val  
+            val = attritionDf.loc[attritionDf["Job Family"] == row["Job Family"]]["Others"].values[0]
+        return val
     
     result["Attrition"] = result.apply(attrition, axis=1)
  
-    def retirement(row):
+    def retirement(row): 
         dataframe = retirementDf.loc[retirementDf["Country of Personnel Area"] == row["Country of Personnel Area"]]
+        retirementDate = row["Birth date"]
         if not(dataframe.empty):
-            val = dataframe.values[0][1]  
+            retirementDate = retirementDate.replace(year=retirementDate.year + int(dataframe.values[0][1]))
         else:
-            val =  retirementDf.loc[retirementDf["Country of Personnel Area"] == "Others"]
-        return val
+            retirementDate = retirementDate.replace(year=retirementDate.year + int(retirementDf.loc[retirementDf["Country of Personnel Area"] == "Others"].values[0][1]))
+        return retirementDate
  
     result["Retirement"] = result.apply(retirement, axis=1)
     return result
 
-def calculateSupply(csvs):
+def calculateSupply(csvs,horizon = 5):
     df = createDF(csvs)
-    print(df)
-    year = datetime.datetime.now().date().year 
+    current = datetime.datetime.now()
+    
+    unique = df.drop_duplicates(subset=["Job title"])
+
+    result = {}
+
+    currentWF = df.loc[(df['Retirement'] > current)] 
+
+    currentFTEs = {}
+
+    for title in unique["Job title"].values:
+        currentWFTitle = currentWF.loc[currentWF["Job title"] == title]
+        currentFTE = sum(list(map(float,currentWFTitle["FTE"].values)))
+        currentFTEs[title] = currentFTE 
+    result[current.year] = currentFTEs
      
-    duplicate = df[df.duplicated("Job title")] 
-    print(duplicate) 
+    forecast = currentFTEs.copy()
+    for year in range(0,horizon):
+        current = current.replace(year=current.year + 1)
+        previous = forecast
+        for title in unique["Job title"].values:
+            lastFTE = previous[title]
+            retiringFTE = currentWF.loc[(df['Retirement'] <= current)].loc[currentWF["Job title"] == title]
+            retiringFTE = sum(list(map(float,retiringFTE["FTE"].values)))
+            result = lastFTE - retiringFTE
+            if result < 0:
+                print("smaller than 0")
+            else:
+                attritionFTE = currentWF.loc
+
+        currentWF = currentWF.loc[(df['Retirement'] > current)]
+        result[current.year] = forecast.copy()
+
+    print(result)
+    
+
     return
 

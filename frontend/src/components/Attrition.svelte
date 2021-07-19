100391107c27479facb6cd3d5628ff4de33fe90e
pgm-arthtemm <arthur.temmerman@student.arteveldehs.be>
2021-07-19 10:58:19 +0200

    added attrition component

---
+++ frontend/src/components/Attrition.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { fetchWorkforceData } from '../api/fetch';
+
+  let things = [];
+  let colNames = [];
+
+  const getData = async () => {
+    const response = await fetchWorkforceData();
+    console.log(response);
+  }
+
+  getData();
+</script>
+
+<table>
+  <thead>
+    <tr>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>
+    {#each things as thing, index (thing.id)}
+      <tr>
+        {#each colNames as col, index}
+          <td>{thing[col]}</td>
+        {/each}
+      </tr>
+    {/each}
+  </tbody>
+</table>
